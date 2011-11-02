a959091f402afe20704842ec5df594b9906f13e2
Camilo Aguilar <camilo.aguilar@gmail.com>
2011-11-02 16:18:25 -0400

    [nodejs] implements module pattern in apikey to avoid api users to mess up the internal object state

--- nodejs/lib/configuration/apikey.js
+++ nodejs/lib/configuration/apikey.js
@@ -1,6 +1,6 @@
 var request = require('superagent');
 var config = require('../config');
-//TODO - cache keys, basically apikey.participation should return the key 
+//TODO - cache keys, basically apikey.participation should return the key
 //right away or retrieve it from the server. The same applies for the
 //configuration key.
 
@@ -10,108 +10,116 @@ var ApiKey = module.exports = function(username, password) {
         'arguments to this constructor.');
     }
 
-    this.auth = 'Basic ' +
+    /**
+     * Authentication string.
+     * @private
+     **/
+    var auth = 'Basic ' +
     new Buffer(username + ':' + password).toString('base64');
-};
 
-ApiKey.prototype.generate = function() {
-    var type;
-    var callback;
+    var my = {};
 
-    var arglen = arguments.length;
-    if (arglen > 2 || arglen < 1) {
-        throw new TypeError('Wrong number of arguments. In order to ' +
-        'invoke this function you need to provide at least one and maximum' +
-        'two arguments.');
-    }
+    my.generate = function() {
+        var type;
+        var callback;
 
-    if (arglen == 2) {
-        type = arguments[0];
-        callback = arguments[1];
-        if (typeof type !== 'string' ||
-            typeof callback !== 'function') {
-            throw new TypeError('When invoking with two arguments, a string ' +
-            'with a API key type is expected as the first argument and a ' +
-            'callback function as the second argument.');
+        var arglen = arguments.length;
+        if (arglen > 2 || arglen < 1) {
+            throw new TypeError('Wrong number of arguments. In order to ' +
+            'invoke this function you need to provide at least one and ' +
+            'maximum two arguments.');
         }
-    }
 
-    if (arglen == 1) {
-        callback = arguments[0];
-        if (typeof callback !== 'function') {
-            throw new TypeError('When invoking with one argument, a ' +
-            'callback function is expected.');
-        }
-    }
-
-    var url = config.baseurl + '/keys';
-
-    if (type) {
-        url += '/' + type;
-    }
-
-    request
-    .post(url)
-    .set('Authorization', this.auth)
-    .end(function(err, res) {
-        if (res.status == 201) {
-            callback(err, res.body);
-        } else {
-            if (!err) {
-                err = res.body;
+        if (arglen == 2) {
+            type = arguments[0];
+            callback = arguments[1];
+            if (typeof type !== 'string' ||
+                typeof callback !== 'function') {
+                throw new TypeError('When invoking with two arguments, a ' +
+                'string with a API key type is expected as the first ' +
+                'argument and a callback function as the second argument.');
             }
-            callback(err);
         }
-    });
-};
 
-ApiKey.prototype.get = function() {
-    var type;
-    var callback;
-
-    var arglen = arguments.length;
-    if (arglen > 2 || arglen < 1) {
-        throw new TypeError('Wrong number of arguments. In order to ' +
-        'invoke this function you need to provide at least one and maximum' +
-        'two arguments.');
-    }
-
-    if (arglen == 2) {
-        type = arguments[0];
-        callback = arguments[1];
-        if (typeof type !== 'string' ||
-            typeof callback !== 'function') {
-            throw new TypeError('When invoking with two arguments, a string ' +
-            'with a API key type is expected as the first argument and a ' +
-            'callback function as the second argument.');
-        }
-    }
-
-    if (arglen == 1) {
-        callback = arguments[0];
-        if (typeof callback !== 'function') {
-            throw new TypeError('When invoking with one argument, a ' +
-            'callback function is expected.');
-        }
-    }
-
-    var url = config.baseurl + '/keys';
-
-    if (type) {
-        url += '/' + type;
-    }
-
-    request
-    .get(url)
-    .set('Authorization', this.auth)
-    .end(function(err, res) {
-        if (res.status == 200) {
-            callback(err, res.body);
-        } else {
-            if (!err) {
-                err = res.body;
+        if (arglen == 1) {
+            callback = arguments[0];
+            if (typeof callback !== 'function') {
+                throw new TypeError('When invoking with one argument, a ' +
+                'callback function is expected.');
             }
-            callback(err);
         }
-    });
+
+        var url = config.baseurl + '/keys';
+
+        if (type) {
+            url += '/' + type;
+        }
+
+        request
+        .post(url)
+        .set('Authorization', auth)
+        .end(function(err, res) {
+            if (res.status == 201) {
+                callback(err, res.body);
+            } else {
+                if (!err) {
+                    err = res.body;
+                }
+                callback(err);
+            }
+        });
+    };
+
+    my.get = function() {
+        var type;
+        var callback;
+
+        var arglen = arguments.length;
+        if (arglen > 2 || arglen < 1) {
+            throw new TypeError('Wrong number of arguments. In order to ' +
+            'invoke this function you need to provide at least one and ' +
+            'maximum two arguments.');
+        }
+
+        if (arglen == 2) {
+            type = arguments[0];
+            callback = arguments[1];
+            if (typeof type !== 'string' ||
+                typeof callback !== 'function') {
+                throw new TypeError('When invoking with two arguments, a ' +
+                'string with a API key type is expected as the first ' +
+                'argument and a callback function as the second argument.');
+            }
+        }
+
+        if (arglen == 1) {
+            callback = arguments[0];
+            if (typeof callback !== 'function') {
+                throw new TypeError('When invoking with one argument, a ' +
+                'callback function is expected.');
+            }
+        }
+
+        var url = config.baseurl + '/keys';
+
+        if (type) {
+            url += '/' + type;
+        }
+
+        request
+        .get(url)
+        .set('Authorization', auth)
+        .end(function(err, res) {
+            if (res.status == 200) {
+                callback(err, res.body);
+            } else {
+                if (!err) {
+                    err = res.body;
+                }
+                callback(err);
+            }
+        });
+    };
+
+    return my;
 };
