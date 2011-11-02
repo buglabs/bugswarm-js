d3e943e2c3bc156c2ab8b8f07e61bada06a83a72
Camilo Aguilar <camilo.aguilar@gmail.com>
2011-11-02 14:54:58 -0400

    [nodejs] adds apikey implementation

--- nodejs/lib/configuration/apikey.js
+++ nodejs/lib/configuration/apikey.js
@@ -1,4 +1,117 @@
 var request = require('superagent');
-var ApiKey = module.exports = function(credentials) {};
-ApiKey.generate = function() {};
-ApiKey.get = function(){};
+var config = require('../config');
+//TODO - cache keys, basically apikey.participation should return the key 
+//right away or retrieve it from the server. The same applies for the
+//configuration key.
+
+var ApiKey = module.exports = function(username, password) {
+    if (!username || !password) {
+        throw new TypeError('You must provide a username and password as ' +
+        'arguments to this constructor.');
+    }
+
+    this.auth = 'Basic ' +
+    new Buffer(username + ':' + password).toString('base64');
+};
+
+ApiKey.prototype.generate = function() {
+    var type;
+    var callback;
+
+    var arglen = arguments.length;
+    if (arglen > 2 || arglen < 1) {
+        throw new TypeError('Wrong number of arguments. In order to ' +
+        'invoke this function you need to provide at least one and maximum' +
+        'two arguments.');
+    }
+
+    if (arglen == 2) {
+        type = arguments[0];
+        callback = arguments[1];
+        if (typeof type !== 'string' ||
+            typeof callback !== 'function') {
+            throw new TypeError('When invoking with two arguments, a string ' +
+            'with a API key type is expected as the first argument and a ' +
+            'callback function as the second argument.');
+        }
+    }
+
+    if (arglen == 1) {
+        callback = arguments[0];
+        if (typeof callback !== 'function') {
+            throw new TypeError('When invoking with one argument, a ' +
+            'callback function is expected.');
+        }
+    }
+
+    var url = config.baseurl + '/keys';
+
+    if (type) {
+        url += '/' + type;
+    }
+
+    request
+    .post(url)
+    .set('Authorization', this.auth)
+    .end(function(err, res) {
+        if (res.status == 201) {
+            callback(err, res.body);
+        } else {
+            if (!err) {
+                err = res.body;
+            }
+            callback(err);
+        }
+    });
+};
+
+ApiKey.prototype.get = function() {
+    var type;
+    var callback;
+
+    var arglen = arguments.length;
+    if (arglen > 2 || arglen < 1) {
+        throw new TypeError('Wrong number of arguments. In order to ' +
+        'invoke this function you need to provide at least one and maximum' +
+        'two arguments.');
+    }
+
+    if (arglen == 2) {
+        type = arguments[0];
+        callback = arguments[1];
+        if (typeof type !== 'string' ||
+            typeof callback !== 'function') {
+            throw new TypeError('When invoking with two arguments, a string ' +
+            'with a API key type is expected as the first argument and a ' +
+            'callback function as the second argument.');
+        }
+    }
+
+    if (arglen == 1) {
+        callback = arguments[0];
+        if (typeof callback !== 'function') {
+            throw new TypeError('When invoking with one argument, a ' +
+            'callback function is expected.');
+        }
+    }
+
+    var url = config.baseurl + '/keys';
+
+    if (type) {
+        url += '/' + type;
+    }
+
+    request
+    .get(url)
+    .set('Authorization', this.auth)
+    .end(function(err, res) {
+        if (res.status == 200) {
+            callback(err, res.body);
+        } else {
+            if (!err) {
+                err = res.body;
+            }
+            callback(err);
+        }
+    });
+};
---
+++ nodejs/test/configuration/apikey.test.js
@@ -0,0 +1,132 @@
+var ApiKey = require('../../bugswarm').configuration.ApiKey;
+
+describe('ApiKey', function(){
+    var user = 'librarytest';
+    var password = 'test';
+    var apikey;
+    var confkey;
+    var partkey;
+    
+    before(function(done) {
+        apikey = new ApiKey(user, password);
+        done();
+    });
+
+    it('should generate configuration and participation keys', function(done){
+        apikey.generate(function(err, keys) {
+            Array.isArray(keys).should.be.true;
+            keys.should.have.length(2);
+            
+            for(var i in keys) {
+                ['user_id', 'created_at', 'status', 'type', 'key']
+                .forEach(function(p) {
+                   keys[i].should.have.property(p); 
+                });
+                keys[i].should.not.have.property('_id');
+            }
+            done();
+        });
+    });
+    
+    it('should generate only the configuration key', function(done) {
+        apikey.generate('configuration', function(err, key) {
+            key.should.be.a('object');
+            
+            ['user_id', 'created_at', 'status', 'type', 'key']
+            .forEach(function(p) {
+                key.should.have.property(p);
+            });
+            
+            key.should.not.have.property('_id');
+            key.type.should.be.eql('configuration');
+            key.key.should.have.lengthOf(40).and.match(/^[a-fA-F0-9]+$/);
+            
+            confkey = key.key;
+            done();
+        });
+    });
+    
+    it('should generate only the participation key', function(done) {
+        apikey.generate('participation', function(err, key) {
+            key.should.be.a('object');
+            
+            ['user_id', 'created_at', 'status', 'type', 'key']
+            .forEach(function(p) {
+                key.should.have.property(p);
+            });
+            
+            key.should.not.have.property('_id');
+            key.type.should.be.eql('participation');
+            key.key.should.have.lengthOf(40).and.match(/^[a-fA-F0-9]+$/);
+            
+            partkey = key.key;
+            done();
+        });
+    });
+    
+    it('should return an error if key type is invalid', function(done) {
+        apikey.generate('invalid_type', function(err, key) {
+            Array.isArray(err).should.be.true;
+            err[0].message.should.be.eql('Instance is not one of the possible '+
+            'values');
+            err[0].attribute.should.be.eql('enum');
+            err[0].details[0].should.be.eql('configuration');
+            err[0].details[1].should.be.eql('participation');
+            done();
+        });
+    });
+    
+    it('should return all the apikeys', function(done){
+        apikey.get(function(err, keys) {
+            Array.isArray(keys).should.be.true;
+            keys.should.have.length(2);
+            
+            for(var i in keys) {
+                ['user_id', 'created_at', 'status', 'type', 'key']
+                .forEach(function(p) {
+                   keys[i].should.have.property(p); 
+                });
+                keys[i].should.not.have.property('_id');
+            }
+            done();
+        });
+    });
+    
+    it('should return only the configuration key', function(done) {
+        apikey.get('configuration', function(err, key) {
+            key.should.be.a('object');
+            
+            ['user_id', 'created_at', 'status', 'type', 'key']
+            .forEach(function(p) {
+                key.should.have.property(p);
+            });
+            
+            key.should.not.have.property('_id');
+            key.type.should.be.eql('configuration');
+            key.key.should.have.lengthOf(40).and.match(/^[a-fA-F0-9]+$/);
+            
+            key.key.should.be.eql(confkey);
+            
+            done();
+        });
+    });
+       
+    it('should return only the participation key', function(done) {
+        apikey.get('participation', function(err, key) {
+            key.should.be.a('object');
+            
+            ['user_id', 'created_at', 'status', 'type', 'key']
+            .forEach(function(p) {
+                key.should.have.property(p);
+            });
+            
+            key.should.not.have.property('_id');
+            key.type.should.be.eql('participation');
+            key.key.should.have.lengthOf(40).and.match(/^[a-fA-F0-9]+$/);
+            
+            key.key.should.be.eql(partkey);
+            
+            done();
+        });
+    });
+});
