099de9c520cb3be77b5a8eff3ccd9f8a4587a1fe
Camilo Aguilar <camilo.aguilar@gmail.com>
2011-11-03 17:16:09 -0400

    [nodejs] adds resource implementation

--- nodejs/lib/configuration/resource.js
+++ nodejs/lib/configuration/resource.js
@@ -1,38 +1,36 @@
 var request = require('superagent');
 var config = require('../config');
+var Swarm = require('./swarm');
 
 var Resource = module.exports = function(apikey, data) {
-    if (!apikey) {
+    if (!apikey || !apikey.length) {
         throw new TypeError('You must provide an API Key as first ' +
         'argument to this constructor.');
     }
-    
-    var url = config.baseurl + '/resources';
-    
-    var my = {};
-    my.create = function() {
-        var callback;
 
+    var url = config.baseurl + '/resources';
+
+    var my = data;
+    my.create = function() {
         var arglen = arguments.length;
         if (arglen > 1 || arglen < 1) {
             throw new TypeError('Wrong number of arguments. In order to ' +
             'invoke this function you need to provide one argument.');
         }
-        
-        if (arglen == 1) {
-            callback = arguments[0];
-            if (typeof callback !== 'function') {
-                throw new TypeError('A callback function is expected as ' +
-                'first argument.');
-            }
+
+        var callback = arguments[0];
+        if (typeof callback !== 'function') {
+            throw new TypeError('A callback function is expected as ' +
+            'first argument.');
         }
-            
+
         request
         .post(url)
         .set('x-bugswarmapikey', apikey)
         .data(data)
         .end(function(err, res) {
             if (res.status == 201) {
+
                 callback(err, res.body);
             } else {
                 if (!err) {
@@ -42,27 +40,36 @@ var Resource = module.exports = function(apikey, data) {
             }
         });
     };
-    
+
     my.update = function() {
         var arglen = arguments.length;
         if (arglen > 2 || arglen < 2) {
             throw new TypeError('Wrong number of arguments. In order to ' +
             'invoke this function you need to provide two arguments.');
         }
-        
+
         var id = arguments[0];
         if (typeof id !== 'string' || !id.length) {
             throw new TypeError('A nonempty string is expected as ' +
             'first argument and it should be the resource id to be ' +
             'updated.');
         }
-        
+
         var callback = arguments[1];
         if (typeof callback !== 'function') {
             throw new TypeError('A callback function is expected as the ' +
             'second argument.');
         }
-        
+
+        /**
+         * Server complains if we send
+         * additional parameters for updates.
+         **/
+        ['id', 'created_at', 'modified_at', 'user_id']
+        .forEach(function(p) {
+            delete data[p];
+        });
+
         request
         .put(url + '/' + id)
         .set('x-bugswarmapikey', apikey)
@@ -78,14 +85,148 @@ var Resource = module.exports = function(apikey, data) {
             }
         });
     };
-    
+
     my.swarms = function() {
-        
+        var arglen = arguments.length;
+        if (arglen > 1 || arglen < 1) {
+            throw new TypeError('Wrong number of arguments. In order to ' +
+            'invoke this function you need to provide one argument.');
+        }
+
+        var callback = arguments[0];
+        if (typeof callback !== 'function') {
+            throw new TypeError('A callback function is expected as the ' +
+            'first argument.');
+        }
+
+        request
+        .get(url + '/' + data.id + '/swarms')
+        .set('x-bugswarmapikey', apikey)
+        .end(function(err, res) {
+            if (res.status == 200) {
+                var swarms = [];
+                for (var i = 0, len = res.body.length; i < len; i++) {
+                    swarms.push(new Swarm(apikey, res.body[i]));
+                }
+                //TODO create Swarms instances and
+                //return them inside an array.
+                callback(err, swarms);
+            } else {
+                if (!err) {
+                    err = res.body;
+                }
+                callback(err);
+            }
+        });
     };
-    
+
     return my;
 };
 
-Resource.destroy = function(){};
-Resource.get = function(id){};
-Resource.prototype.swarms = function(){};
+Resource.get = function(){
+    var url = config.baseurl + '/resources';
+
+    var apikey, id, callback;
+
+    var arglen = arguments.length;
+    if (arglen > 3 || arglen < 2) {
+        throw new TypeError('Wrong number of arguments. In order to ' +
+        'invoke this function you need to provide at least two and ' +
+        'maximum three arguments.');
+    }
+
+    if (arglen == 2) {
+        apikey = arguments[0];
+        callback = arguments[1];
+        if (typeof apikey !== 'string' ||
+            !apikey.length ||
+            typeof callback !== 'function') {
+            throw new TypeError('When invoking with two arguments, a ' +
+            'string with an API Key is expected as the first ' +
+            'argument and a callback function as the second argument.');
+        }
+    } else if (arglen == 3) {
+        apikey = arguments[0];
+        id = arguments[1];
+        callback = arguments[2];
+        if (typeof apikey !== 'string' ||
+            !apikey.length ||
+            typeof id !== 'string' ||
+            !id.length ||
+            typeof callback !== 'function') {
+            throw new TypeError('When invoking with three arguments, a ' +
+            'string with an API Key is expected as the first ' +
+            'argument, a string with a resource id as the second '+
+            'and a callback function as the third argument.');
+        }
+    }
+
+    if (id) {
+        url += '/' + id;
+    }
+
+    request
+    .get(url)
+    .set('x-bugswarmapikey', apikey)
+    .end(function(err, res) {
+        if (res.status == 200) {
+            if(id) {
+                var resource = new Resource(apikey, res.body);
+                callback(err, resource);
+            } else {
+                var resources = [];
+                for (var i = 0, len = res.body.length; i < len; i++) {
+                    resources.push(new Resource(apikey, res.body[i]));
+                }
+                callback(err, resources);
+            }
+        } else {
+            if (!err) {
+                err = res.body;
+            }
+            callback(err);
+        }
+    });
+};
+
+Resource.destroy = function(){
+    var url = config.baseurl + '/resources';
+
+    var arglen = arguments.length;
+    if (arglen > 3 || arglen < 3) {
+        throw new TypeError('Wrong number of arguments. In order to ' +
+        'invoke this function you need to provide two arguments.');
+    }
+
+    var apikey = arguments[0];
+    if (typeof apikey !== 'string' || !apikey.length) {
+        throw new TypeError('A nonempty string is expected as ' +
+        'first argument and it should be your API Key updated.');
+    }
+
+    var id = arguments[1];
+    if (typeof id !== 'string' || !id.length) {
+        throw new TypeError('A nonempty string is expected as ' +
+        'second argument and it should be the resource id to be destroyed.');
+    }
+
+    var callback = arguments[2];
+    if (typeof callback !== 'function') {
+        throw new TypeError('A callback function is expected as the ' +
+        'second argument.');
+    }
+    
+    request
+    .del(url + '/' + id)
+    .set('x-bugswarmapikey', apikey)
+    .end(function(err, res) {
+        if (res.status == 204) {
+            callback();
+        } else {
+            if (!err) {
+                err = res.body;
+            }
+            callback(err);
+        }
+    });
+};
--- nodejs/test/configuration/resource.test.js
+++ nodejs/test/configuration/resource.test.js
@@ -4,29 +4,29 @@ var ApiKey = require('../../bugswarm').configuration.ApiKey;
 describe('Resource', function(){
     var apikey;
     var resourceId;
-    
+
     before(function(done) {
-        new ApiKey('librarytest', 'test').generate('configuration', 
+        new ApiKey('librarytest', 'test').generate('configuration',
         function(err, key) {
             apikey = key.key;
             done();
         });
     });
-    
+
     it('should create a resource', function(done) {
         var data = {
             name: 'my resource',
             description: 'my description',
             machine_type: 'bug'
         };
-        
+
         var _resource = new Resource(apikey, data);
         _resource.create(function(err, resource) {
             resource.should.be.a('object');
-            
+
             resource.should.have.property('id');
             resourceId = resource.id;
-            
+
             resource.name.should.be.eql('my resource');
             resource.description.should.be.eql('my description');
             resource.machine_type.should.be.eql('bug');
@@ -34,42 +34,73 @@ describe('Resource', function(){
             done();
         });
     });
-    
-    it('should update a resource', function(done) {
-        var data = {
-            name: 'my resource modified',
-            description: 'my description modified',
-            machine_type: 'pc'
-        };
 
-        var _resource = new Resource(apikey, data);
-        _resource.update(resourceId, function(err, resource) {
+    it('should get a specific resource by id', function(done) {
+        Resource.get(apikey, resourceId, function(err, resource) {
             resource.should.be.a('object');
             resource.should.have.property('id');
             resource.id.should.be.eql(resourceId);
-            resource.name.should.be.eql('my resource modified');
-            resource.description.should.be.eql('my description modified');
-            resource.machine_type.should.be.eql('pc');
-            resource.should.have.property('created_at');
-            resource.should.have.property('modified_at');
+            //console.log(resource);
+            ['name', 'user_id', 'id', 'description', 'machine_type',
+             'created_at'].forEach(function(p) {
+                resource.should.have.property(p);
+            });
             done();
         });
     });
-    
-    it('should destroy a resource', function(done) {
-        done();
+
+    it('should update a resource', function(done) {
+        Resource.get(apikey, resourceId, function(err, _resource) {
+            _resource.name = 'my resource modified';
+            _resource.description = 'my description modified';
+            _resource.machine_type = 'pc';
+
+            _resource.update(resourceId, function(err, resource) {
+                resource.should.be.a('object');
+                resource.should.have.property('id');
+                resource.id.should.be.eql(resourceId);
+                resource.name.should.be.eql('my resource modified');
+                resource.description.should.be.eql('my description modified');
+                resource.machine_type.should.be.eql('pc');
+                resource.should.have.property('created_at');
+                resource.should.have.property('modified_at');
+                done();
+            });
+        });
     });
-    
+
     it('should get all the existing resources', function(done) {
-        done();
+        Resource.get(apikey, function(err, resources) {
+            Array.isArray(resources).should.be.eql(true);
+            resources.length.should.be.above(1);
+            resources.forEach(function(resource) {
+                ['name', 'user_id', 'id', 'description', 'machine_type',
+                'created_at'].forEach(function(p) {
+                resource.should.have.property(p);
+                });
+            });
+            done();
+        });
     });
-    
-    it('should get a specific resource by id', function(done) {
-        done();
-    });
-    
+
     it('should return the list of swarms where the resource is ' +
     'a participant', function(done) {
-        done();
+        Resource.get(apikey, resourceId, function(err, resource) {
+            resource.swarms(function(err, swarms) {
+                Array.isArray(swarms).should.be.eql(true);
+                swarms.length.should.be.eql(0);
+                done();
+            });
+        });
+    });
+
+    it('should destroy a resource', function(done) {
+        Resource.destroy(apikey, resourceId, function(err) {
+            //TODO
+            //uncomment when https://github.com/visionmedia/should.js/issues/25
+            //is fixed.
+            //should.exist(err);
+            done();
+        });
     });
 });
