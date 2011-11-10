2ac1af488d52b18bdcd131c43e8af3fe1b20ad97
John Connolly <john@buglabs.net>
2011-11-10 17:45:41 -0500

    removing duplicate implementation of java api.  fixes https://github.com/buglabs/bugswarm-api/issues/11

--- java/Swarm/src/main/com/buglabs/swarm/Callback.java
+++
@@ -1,8 +0,0 @@
-package com.buglabs.swarm;
-
-/**
- * Interface that end users should implement
- */
-public interface Callback {
-	void onMessage(Object object); //ideally this will be a JSONObject (simple json) or using http://wiki.fasterxml.com/JacksonDataBinding
-}
